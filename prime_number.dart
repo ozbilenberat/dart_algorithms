052bf859a17e8623c2636d33759bec275a3a6523
beratozb <ozbilenberat@gmail.com>
2022-02-05 00:42:58 +0300

    sadeleştirme yapıldı

--- prime_number.dart
+++ prime_number.dart
@@ -3,22 +3,22 @@ algoritma örneği
 */
 
 void main() {
-  print(primenumbers(10, 200));
+  print(primeNumbers(10, 200));
 }
 
-List<int> primenumbers(int sayi1, int sayi2) {
+List<int> primeNumbers(int number1, int number2) {
   List<int> allprimeNumbers = [];
 
   // ignore: unused_local_variable
   bool prime = true;
-  for (sayi1; sayi1 < sayi2; sayi1++) {
-    for (int n = 2; n < sayi1; n++) {
-      if (sayi1 % n == 0) {
+  for (number1; number1 < number2; number1++) {
+    for (int n = 2; n < number1; n++) {
+      if (number1 % n == 0) {
         prime = false;
         break;
       }
-      if (prime = true && (n == sayi1 - 1)) {
-        allprimeNumbers.add(sayi1);
+      if (prime = true && (n == number1 - 1)) {
+        allprimeNumbers.add(number1);
       }
     }
   }
