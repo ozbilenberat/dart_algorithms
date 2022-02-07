5efb0ec6de0089e143e40a29fc2d46ae2ce2e352
beratozb <ozbilenberat@gmail.com>
2022-02-08 00:16:00 +0300

    kod sadeleştirildi liste kullanımı çıkarıldı

--- listmath.dart
+++ listmath.dart
@@ -1,12 +1,10 @@
+/* 1 ve 100 arasında iki katının bir fazlası 
+beşe tam bölünebilen tam sayıları yazdıran algoritma
+*/
 void main() {
-  var allNumbers = Iterable<int>.generate(100).toList();
-  List<int> finalNumbers = [];
-  for (int i = 0; i < allNumbers.length; i++) {
-    var currentIndex = (allNumbers[i]);
-    int performNumber = currentIndex * 2 + 1;
-    if (performNumber % 5 == 0) {
-      finalNumbers.add(currentIndex);
+  for (int i = 0; i < 100; i++) {
+    if ((i * 2 + 1) % 5 == 0) {
+      print(i);
     }
   }
-  print(finalNumbers);
 }
